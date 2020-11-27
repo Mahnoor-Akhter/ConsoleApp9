9888e497ee3e33b019ac5210f40f63fe0e609654
Mah Noor <S00199547@mail.itsligo.ie>
2020-11-27 01:12:53 +0000

    Add project files.

---
+++ ConsoleApp9/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+	public class Player
+	{
+		public int ID { get; set; }
+
+		public string PlayerName { get; set; }
+		public int Score { get; set; }
+	}
+}
